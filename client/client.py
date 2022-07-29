67d05f685efbb4f5413193337cd94ce2b87f1bc3
Timurlan Abylkasymov <93774365+Timoshka996@users.noreply.github.com>
2022-07-29 16:19:00 +0600

    Update client.py

--- client/client.py
+++ client/client.py
@@ -18,6 +18,7 @@ fp = urllib.request.urlopen("http://localhost:1234/")
 # 'encodedContent' соответствует закодированному ответу сервера ('index.html').
 # 'decodedContent' соответствует раскодированному ответу сервера (тут будет то, что мы хотим вывести на экран).
 
+
 encodedContent = fp.read()
 decodedContent = encodedContent.decode("utf8")
 
